ef35fbe784309307b71f4cbfd552d8fde363f5d2
ZHANGENSEA <149954066+ZHANGENSEA@users.noreply.github.com>
2024-01-11 21:09:48 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,112 +1,115 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
-def homepage():
-    return jsonify({"message": "Welcome to my shop!"})
+def hello_world():
+  return jsonify({
+      "message":
+      "Welcome to my API created with Flask! :) go to /api/books to see the list of books"
+  })
 
-stockList = [
-    {
-        'id': 0,
-        'product': 'fraise',
-        'reference': 'fruit-01',
-        'photo': 'https://img.freepik.com/photos-gratuite/baie-fraise-levitation-fond-blanc_485709-57.jpg?w=740&t=st=1699203655~exp=1699204255~hmac=e31baca39bcdc608e322b8562bf1faede61502f794630e9f37990202e8590519',
-        'quantity': 1000
-    },
-    {
-        'id': 1,
-        'product': 'orange',
-        'reference': 'fruit-02',
-        'photo': 'https://img.freepik.com/photos-gratuite/orange-blanc-blanc_144627-16571.jpg?w=740&t=st=1699203776~exp=1699204376~hmac=ffafcab7fe3ceb6e05f868957618977bf12e8f7dec91566e7d08a2fa09618693',
-        'quantity': 100
-    },
-    {
-        'id': 2,
-        'product': 'pomme',
-        'reference': 'fruit-03',
-        'photo': 'https://img.freepik.com/photos-gratuite/pomme-verte-isolee-blanc_2829-9600.jpg?w=740&t=st=1699203803~exp=1699204403~hmac=6cbf2173de06a899baed27be075118d4ee5bf68618b66ef4ab450d34a006871f',
-        'quantity': 5000
-    },
-    {
-        'id': 3,
-        'product': 'concombre',
-        'reference': 'legume-01',
-        'photo': 'https://img.freepik.com/photos-gratuite/concombre-vert_144627-21625.jpg?w=996&t=st=1699203946~exp=1699204546~hmac=5e649ffa5ba28a408f6a3f747d89d4791b9daf4ea7c8be50f2678db79db61afb',
-        'quantity': 500
-    },
-    {
-        'id': 4,
-        'product': 'courgette',
-        'reference': 'legume-02',
-        'photo': 'https://img.freepik.com/photos-gratuite/arrangement-angle-eleve-courgettes_23-2148917717.jpg?w=900&t=st=1699204040~exp=1699204640~hmac=cda2ddea0a268ae0a5f54bfc2e7f109e72c362eef09bb178b5fbb1be9bdc87c9',
-        'quantity': 1500
-    },
-    {
-        'id': 5,
-        'product': 'carotte',
-        'reference': 'legume-03',
-        'photo': 'https://img.freepik.com/photos-gratuite/fond-carottes_1339-3261.jpg?w=900&t=st=1699204128~exp=1699204728~hmac=b6ea4d35e78e8eef9d02841a85f2ec6bf8ad2362bc098e536c5b0499b602e3e5',
-        'quantity': 5000
+
+book_list = [{
+    'id':
+    1,
+    'title':
+    'titre 01',
+    'imageUrl':
+    'https://images.pexels.com/photos/1049298/pexels-photo-1049298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+}, {
+    'id':
+    2,
+    'title':
+    'test 03',
+    'imageUrl':
+    'https://images.pexels.com/photos/933054/pexels-photo-933054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+}, {
+    'id':
+    3,
+    'title':
+    'Woods',
+    'imageUrl':
+    'https://images.pexels.com/photos/418831/pexels-photo-418831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+}, {
+    'id':
+    4,
+    'title':
+    'Nice Beach',
+    'imageUrl':
+    'https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+}]
+
+
+# route that retuns books
+@app.route('/api/books', methods=["GET", "POST"])
+def books():
+  # print(request.method)
+  # evaluate request method if "GET" return all the books
+  # if "POST" we add a new book to the list
+  if request.method == "GET":
+    return jsonify(book_list)
+  else:
+    req_body = request.json
+    # caluclate number of books
+    no_de_livres = len(book_list)
+    # calculate new id
+    new_id = no_de_livres + 1
+
+    # new book creation
+    new_book = {
+        'title': req_body['title'],
+        'imageUrl': req_body['imageUrl'],
+        'id': new_id
     }
-]
+    # add new book to tle list
+    book_list.append(new_book)
 
-@app.route('/api/v1/stocks', methods=["GET", "POST"])
-def stocks():
+    # return the new book with its new id
+    return jsonify(new_book), 201
 
+
+# route that retuns one book
+@app.route('/api/books/<int:id>', methods=["GET", "DELETE", "PUT"])
+def one_book(id):
+
+  # try to find a book
+  try:
+
+    # if GET method is sent
     if request.method == "GET":
-        return jsonify(stockList)
-    elif request.method == "POST":
-        req_body = request.json
-        nextId = len(stockList)
+      for book in book_list:
+        print(book)
+        if book['id'] == id:
+          return jsonify(book)
 
-        newStock = {
-            'id':nextId,
-            'product': req_body['product'],
-            'reference': req_body['reference'],
-            'photo': req_body['photo'],
-            'quantity': req_body['quantity']
-        } 
-        stockList.append(newStock)
+    # if DELETE method is sent
+    elif request.method == "DELETE":
+      for book in book_list:
+        print(book)
+        if book['id'] == id:
+          book_list.remove(book)
+          return jsonify({"msg": "book deleted"})
+    elif request.method == "PUT":
+      for book in book_list:
+        print(book)
+        if book['id'] == id:
+          book['title'] = request.json['title']
+          book['imageUrl'] = request.json['imageUrl']
 
-        return jsonify(newStock), 201
-    else :
-        return jsonify ({"error": "unknown error"}), 404
+      return jsonify({"msg": "book updated"})
 
-@app.route('/api/v1/stock/<int:id>', methods=["GET", "PUT", "DELETE"])
-def stock(id):
+  except Exception as e:
+    print(book_list, id)
+    print(str(e))
 
-    try:
-        stock = stockList[id]
-        if request.method == "GET":
-            return jsonify(stock)
-        elif request.method == "PUT":
-            updated = False
-            if request.json['product']!=stockList[id]['product']:
-                stockList[id]['product'] = request.json['product']
-                updated = True
-            if request.json['reference']!=stockList[id]['reference']:
-                stockList[id]['reference'] = request.json['reference']
-                updated = True
-            if request.json['photo']!=stockList[id]['photo']:
-                stockList[id]['photo'] = request.json['photo']
-                updated = True
-            if request.json['quantity']!=stockList[id]['quantity']:
-                stockList[id]['quantity'] = request.json['quantity']
-                updated = True
-            if (updated):
-                return jsonify({"message": "stock updated"})
-            else:
-                return jsonify({"message": "stock not updated"})
-            
-        elif request.method == "DELETE":
-            stockList.pop(id)
-            return jsonify({"message": "stock deleted"})
-        else :
-            return jsonify ({"error": "unknown error"}), 404
-        
-    except Exception as e:
-        return jsonify ({"error": e}), 404
+    return jsonify({"msg": "Book not found", "error": str(e)}), 404
+
+  # evaluate request method if "GET" return all the books
+  # if "POST" we add a new book to the list
+
 
 app.debug = True
-app.run(host='127.0.0.1', port=8080)
+app.run(host='0.0.0.0', port=5000)
